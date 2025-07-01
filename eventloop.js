ff87046d09170b0d9eef832c4cec528181c470e0
Prerit <preritsiwach1@gmail.com>
2025-07-01 15:04:11 +0530

    Ok

--- eventloop.js
+++ eventloop.js
@@ -6,4 +6,14 @@ setTimeout(()=>{
 setImmediate(()=>{
     console.log("Immediate callback")
 })
+
+fs.readFile("demo.txt",(data)=>{
+    console.log("poll phase callback");
+    setTimeout(()=>{
+        console.log("timer 2")
+    },0)
+    setImmediate(()=>{
+        console.log("immd2")
+    })
+})
 console.log("end")
