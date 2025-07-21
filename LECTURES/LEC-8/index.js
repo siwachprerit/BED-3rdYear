daf4284a7fd9b75d16c08f7ed2fea1bb7ddabccc
Prerit Siwach <preritsiwach@Prerits-MacBook-Pro.local>
2025-07-21 15:26:48 +0530

    21-07-2025

--- LECTURES/LEC-8/index.js
+++ LECTURES/LEC-8/index.js
@@ -1,10 +1,33 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
 
-// app.get('/',(req,res)=>{
-//     res.send('Hello World!');
-// })
+
+app.get('/', (req, res) => {
+//   res.send('<h1>Welcome to the Home Page</h1>'); //response header set hota hai
+//   res.send("Hello World");
+//   res.sendFile(__dirname + '/index.html');
+//   res.json({
+//     name: "John Doe",
+//     age: 30,
+//   })
+//   res.end("hi");//response header set nhi hota
+});
+
+//path-variable
+//1. Query Parameter
+// app.get("/watch",(req, res) => {
+//     let videoId=req.query.v;
+//     let nId = req.query.n;
+//     res.send("id got it"+" "+videoId+" "+nId);
+// });
+
+//2.Params
+app.get("/watch/:v/video/:n",(req, res) => {
+    console.log(req.params.v);
+    console.log(req.params.n);
+    res.send("id got it !!!!!!");
+});
 
 app.listen(3000,function(){
-    console.log("Server Started");
+    console.log("Server started");
 });
