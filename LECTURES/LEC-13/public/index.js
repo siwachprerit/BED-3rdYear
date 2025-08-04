65c3b06bb40441dd2d55daa9d8842905ad89bd9d
Prerit <preritsiwach1@gmail.com>
2025-08-04 15:36:22 +0530

    New

---
+++ LECTURES/LEC-13/public/index.js
@@ -0,0 +1,35 @@
+const ul = document.querySelector(".user-container");
+function getUsersData(URL){
+    fetch(URL)
+    .then((res)=>{
+        console.log(res)
+        return res.json()
+    })
+    .then((data)=>{
+        console.log(data)
+        data.forEach(user => {
+            displayUser(user)
+        });
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+}
+
+function displayUser(user){
+    const li = document.createElement("li");
+    li.innerHTML=`
+     <li class="user-item" style="display:flex">
+        <div class="user-info">
+            <h1>${user.name}</h1>
+            <p>${user.username}</p>
+        </div>
+        <div class="user-btn">
+            <button class="user-delete">❤</button>
+            <button class="user-edit">🤣</button>
+        </div>
+    </li>
+    `
+    ul.appendChild(li);
+}
+getUsersData("http://localhost:3000/users")
